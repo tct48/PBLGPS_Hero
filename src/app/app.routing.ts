b0a24d167cd36932d0cf7925df1d2a693fd22a52
ketarWorapol <s6102041520131@email.kmutnb.ac.th>
2020-05-01 11:50:20 +0700

    test routing commit

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,14 +1,15 @@
 import { Routes, RouterModule } from '@angular/router';
 import { AppURL } from './app.url';
+import { AuthenticationModule } from './authentication/authentication.module'
 import { SigninComponent } from './components/signin/signin.component';
-import { SignupComponent } from './components/signup/signup.component';
 import { HomepageComponent } from './components/homepage/homepage.component';
+import { SignupComponent } from './components/signup/signup.component';
 
 const RouterLists: Routes = [
   { path: '', redirectTo: AppURL.Login, pathMatch:'full' },
   { path: AppURL.Login, component: SigninComponent },
-  { path : AppURL.Register, component: SignupComponent},
   { path: AppURL.Home, component: HomepageComponent},
+  { path: AppURL.Register, component: SignupComponent},
   {
     path: AppURL.Authen, loadChildren: './authentication/authentication.module#AuthenticationModule',
     // canActivate:[AuthenticationGuard] ./authentication/authentication.module#AuthenticationModule
